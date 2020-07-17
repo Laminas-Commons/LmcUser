93bdf62d549956183d54f1716ab07c5857cab5d7
Mat Wright <mat@bstechnologies.com>
2020-07-17 15:56:05 +0200

    update test method signature to correct type

--- tests/LmcUserTest/Authentication/Adapter/TestAsset/AbstractAdapterExtension.php
+++ tests/LmcUserTest/Authentication/Adapter/TestAsset/AbstractAdapterExtension.php
@@ -4,10 +4,11 @@ namespace LmcUserTest\Authentication\Adapter\TestAsset;
 
 use Laminas\EventManager\EventInterface;
 use LmcUser\Authentication\Adapter\AbstractAdapter;
+use LmcUser\Authentication\Adapter\AdapterChainEvent;
 
 class AbstractAdapterExtension extends AbstractAdapter
 {
-    public function authenticate(EventInterface $e)
+    public function authenticate(AdapterChainEvent $e)
     {
     }
 }
